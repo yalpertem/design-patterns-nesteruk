c75a068c14a48241f0d4574ef0680e98fe377ecf
Yigitalp Ertem <yalpertem@gmail.com>
2020-07-16 14:03:34 +0300

    Iterator example added.

---
+++ DesignPatterns.UnitTests/Iterator/BinaryTreeInOrder/CreatureTests.cs
@@ -0,0 +1,24 @@
+﻿using DesignPatterns.Iterator.ArrayBackedProperties;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.UnitTests.Iterator.BinaryTreeInOrder
+{
+    [TestFixture]
+    public class CreatureTests
+    {
+        [Test]
+        public void AverageStat_SetArrayBackedProperties_ValidateResult()
+        {
+            var creature = new Creature()
+            {
+                Agility = 5,
+                Intelligence = 10,
+                Strength = 3
+            };
+            Assert.That(creature.AveregeStat, Is.EqualTo(6));
+        }
+    }
+}
---
+++ DesignPatterns/Iterator/ArrayBackedProperties/Creature.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignPatterns.Iterator.ArrayBackedProperties
+{
+    public class Creature : IEnumerable<int>
+    {
+        private readonly int[] _stats = new int[3];
+        private const int _strength = 0;
+        private const int _agility = 1;
+        private const int _intelligence = 2;
+        public int Strength
+        {
+            get => _stats[_strength];
+            set => _stats[_strength] = value;
+        }
+
+        public int Agility
+        {
+            get => _stats[_agility];
+            set => _stats[_agility] = value;
+        }
+
+        public int Intelligence
+        {
+            get => _stats[_intelligence];
+            set => _stats[_intelligence] = value;
+        }
+
+        public int this[int index]
+        {
+            get => _stats[index];
+            set => _stats[index] = value ;
+        }
+
+        public double AveregeStat => _stats.Average();
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return _stats.AsEnumerable().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
